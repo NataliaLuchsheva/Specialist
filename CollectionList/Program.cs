f2faad8ebdaf5f3a98846c23b3d8d9f4d2bc3310
Лучшева Наталья <n.luchsheva@dmbasis.ru>
2020-11-10 18:18:30 +0300

    День 2.Задания.

---
+++ CollectionList/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> persons = new List<string>();
+            int num = 0;
+            
+            while (true)
+            {
+                num++;
+                Console.Write("Введите имя {0}: ", num);
+                string name = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(name))
+                    persons.Add(name);
+                else
+                    break;
+            }
+
+            persons.Sort();
+
+            foreach (string name in persons)
+                Console.WriteLine(name);
+        }
+    }
+}
---
+++ Day2_Arreys/Array.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Day2_Arreys
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер будущего массива:");
+            int r = int.Parse(Console.ReadLine());
+
+            if (r <= 0)
+            { 
+                Console.WriteLine("Размер некорректный");
+                return;
+            }
+
+            int[] arr = new int[r];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Введите {0} элемент (число) :", i+1);
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            // сортировка и суммирование всех элементов
+            int temp;
+            int summ = 0;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                        summ += temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Максимальный элемент: {0}", arr[^1]);
+            Console.WriteLine("Минимальный элемент: {0}", arr[0]);
+
+            Double avgRes= summ / r;
+            Console.WriteLine("Среднее значение элементов: {0:F3}", avgRes);
+
+
+        }
+    }
+}
+
+
+
+ 
